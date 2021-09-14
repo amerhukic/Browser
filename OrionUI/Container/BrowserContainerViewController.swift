3dbdcc8e34ee322f39bc73210c3f587d55b20dde
Amer Hukic <hukicamer@gmail.com>
2021-09-14 13:57:16 +0200

    Implement keyboard hiding/showing notification handling

--- OrionUI/Container/BrowserContainerViewController.swift
+++ OrionUI/Container/BrowserContainerViewController.swift
@@ -17,12 +17,29 @@ class BrowserContainerViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    setupKeyboardObservers()
     openNewTab()
   }
 }
 
 // MARK: Helper methods
 private extension BrowserContainerViewController {
+  func setupKeyboardObservers() {
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(keyboardWillShow(_:)),
+      name: UIResponder.keyboardWillShowNotification,
+      object: nil
+    )
+    
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(keyboardWillHide(_:)),
+      name: UIResponder.keyboardWillHideNotification,
+      object: nil
+    )
+  }
+  
   func openNewTab() {
     let tabViewController = BrowserTabViewController()
     tabViewControllers.append(tabViewController)
@@ -32,5 +49,56 @@ private extension BrowserContainerViewController {
     }
     addChild(tabViewController)
     tabViewController.didMove(toParent: self)
+  }
+}
+
+// MARK: Keyboard handling
+private extension BrowserContainerViewController {
+  @objc func keyboardWillShow(_ notification: NSNotification) {
+    animateWithKeyboard(notification: notification) { keyboardFrame in
+      print(keyboardFrame)
+    }
+  }
+  
+  @objc func keyboardWillHide(_ notification: NSNotification) {
+    animateWithKeyboard(notification: notification) { keyboardFrame in
+      print(keyboardFrame)
+    }
+  }
+}
+
+extension UIViewController {
+  func animateWithKeyboard(
+    notification: NSNotification,
+    animations: ((_ keyboardFrame: CGRect) -> Void)?
+  ) {
+    // Extract the duration of the keyboard animation
+    let durationKey = UIResponder.keyboardAnimationDurationUserInfoKey
+    let duration = notification.userInfo![durationKey] as! Double
+    
+    // Extract the final frame of the keyboard
+    let frameKey = UIResponder.keyboardFrameEndUserInfoKey
+    let keyboardFrameValue = notification.userInfo![frameKey] as! NSValue
+    
+    // Extract the curve of the iOS keyboard animation
+    let curveKey = UIResponder.keyboardAnimationCurveUserInfoKey
+    let curveValue = notification.userInfo![curveKey] as! Int
+    let curve = UIView.AnimationCurve(rawValue: curveValue)!
+    
+    // Create a property animator to manage the animation
+    let animator = UIViewPropertyAnimator(
+      duration: duration,
+      curve: curve
+    ) {
+      // Perform the necessary animation layout updates
+      animations?(keyboardFrameValue.cgRectValue)
+      
+      // Required to trigger NSLayoutConstraint changes
+      // to animate
+      self.view?.layoutIfNeeded()
+    }
+    
+    // Start the animation
+    animator.startAnimation()
   }
 }
